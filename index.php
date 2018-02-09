b51088b17ec3149f63d985ca468bb4f49316a998
Ethelia Lung <exl8210@rit.edu>
2018-02-09 18:03:28 -0500

    Initial commit
    Setting up the index HTML file and the CSS file.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+        
+    <head>
+        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <title>Niyama</title>
+        
+        <!-- styles -->
+        <link href="assets/style.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Karla:400,400i,700,700i" rel="stylesheet">        
+        
+        <!-- script -->
+        <script type="text/javascript">
+            "use strict";
+            
+            window.onload = init;
+            
+            function init() {
+                console.log("init() called");
+            }
+        </script>
+    
+    </head>
+    
+    <body>
+    
+        <h1>H1</h1>
+        <h2>H2</h2>
+        
+        <p>Body paragraph. <strong>Bold</strong>, <em>italic</em>, <u>underlined</u>.</p>
+        
+        <a class="button">Meep.</a>
+        
+    </body>
+    
+</html>
