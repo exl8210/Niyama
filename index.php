8b057323c6ee7c849f6dbe6c2b51d82c025b8c52
Ethelia Lung <exl8210@rit.edu>
2018-02-18 18:20:25 -0500

    Analytics, PHP includes
    Added Google Analytics code to it, set up header.php and footer.php for
    future use of PHP includes.

--- index.php
+++ index.php
@@ -1,39 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-        
-    <head>
-        
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        <title>Niyama</title>
-        
-        <!-- styles -->
-        <link href="assets/style.css" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Karla:400,400i,700,700i" rel="stylesheet">        
-        
-        <!-- script -->
-        <script type="text/javascript">
-            "use strict";
-            
-            window.onload = init;
-            
-            function init() {
-                console.log("init() called");
-            }
-        </script>
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Niyama</title>
+
+    <!-- styles -->
+    <link href="assets/style.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Karla:400,400i,700,700i" rel="stylesheet">
+
+    <!-- script -->
+    <script type="text/javascript">
+        "use strict";
+
+        window.onload = init;
+
+        function init() {
+            console.log("init() called");
+        }
+
+    </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110221230-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-110221230-2');
+
+    </script>
+
+
+</head>
+
+<body>
     
-    </head>
+    <header>
     
-    <body>
     
-        <h1>H1</h1>
-        <h2>H2</h2>
-        
-        <p>Body paragraph. <strong>Bold</strong>, <em>italic</em>, <u>underlined</u>.</p>
-        
-        <a class="button">Meep.</a>
-        
-    </body>
+    </header>
     
+    <section>
+    
+    
+    </section>
+
+    <h1>H1</h1>
+    <h2>H2</h2>
+
+    <p>Body paragraph. <strong>Bold</strong>, <em>italic</em>, <u>underlined</u>.</p>
+
+    <a class="button">Meep.</a>
+    
+    <footer>
+    
+    
+    </footer>
+
+</body>
+
 </html>
