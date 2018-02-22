4c6e26951e4d57f80a7ec4ac2a66038a3d479290
Ethelia Lung <exl8210@rit.edu>
2018-02-21 22:18:01 -0500

    Finished skeleton of info page
    Added in all the copy and text, including images.

--- information.php
+++ information.php
@@ -11,11 +11,95 @@
     <div class="u-cf clear"></div>
 
     <!-- Apps -->
-    <div class="row" id="more-info-intro">        
+    <div class="row" id="apps">        
         <div class="offset-by-one five columns">
             <h2>Apps</h2>
+        </div>
+        
+        <div class="u-cf"></div>
+        
+        <div class="offset-by-one five columns">
+            <img src="assets/media/apps/headspace.jpg" class="u-pull-left" />
             
-            <p>Everyone has happiness inside them, and we rise by lifting others. Here are some resources you can look to in your daily life to learn more about the underlying psychology of positivity, and what you can use to promote mindfulness.</p>
+            <h3>Headspace</h3>
+            <p>Headspace is a digital service that provides guided meditation sessions and mindfulness training. Its content can be accessed online, or via their mobile apps.</p>
+        </div>
+        
+        <div class="offset-by-one five columns">
+            <img src="assets/media/apps/happify.jpg" class="u-pull-left" />
+            
+            <h3> Happify</h3>
+            <p>Happify is the single destination for effective, evidence-based solutions for better emotional health and wellbeing in the 21st century.</p>
+        </div>
+    </div>
+
+    <div class="u-cf clear"></div>
+
+    <!-- Talks -->
+    <div class="row" id="ted-talks">        
+        <div class="offset-by-one five columns">
+            <h2>Apps</h2>
+        </div>
+        
+        <div class="u-cf"></div>
+        
+        <div class="offset-by-one three columns">
+            <img src="assets/media/headspace.jpg" />
+            
+            <h3>The new era of positive psychology</h3>
+            <p>Martin Seligman talks about psychology -- as a field of study and as it works one-on-one with each patient and each practitioner. As it moves beyond a focus on disease, what can modern psychology help us to become?</p>
+        </div>
+        
+        <div class="offset-by-one three columns">
+            <img src="assets/media/happify.jpg" />
+            
+            <h3>The surprising science of happiness</h3>
+            <p>Dan Gilbert, author of "Stumbling on Happiness," challenges the idea that we'll be miserable if we don't get what we want. Our "psychological immune system" lets us feel truly happy even when things don't go as planned.</p>
+        </div>
+        
+        <div class="offset-by-one three columns">
+            <img src="assets/media/happify.jpg" />
+            
+            <h3>The positive effects of positive emotions</h3>
+            <p>Positive psychology researcher Jennifer Stellar illuminates how truly awe-inspiring experiences may mean more than we realize for our physical and mental health.</p>
+        </div>
+        
+        <div class="offset-by-one three columns">
+            <img src="assets/media/happify.jpg" />
+            
+            <h3>Everyday leadership</h3>
+            <p>We have all changed someone's life -- usually without even realizing it. In this funny talk, Drew Dudley calls on all of us to celebrate leadership as the everyday act of improving each other's lives.</p>
+        </div>
+        
+        <div class="offset-by-one three columns">
+            <img src="assets/media/happify.jpg" />
+            
+            <h3>Why you should talk to strangers</h3>
+            <p>"When you talk to strangers, you're making beautiful interruptions into the expected narrative of your daily life -- and theirs," says Kio Stark. In this delightful talk, Stark explores the overlooked benefits of pushing past our default discomfort when it comes to strangers and embracing those fleeting but profoundly beautiful moments of genuine connection.</p>
+        </div>
+    </div>
+
+    <div class="u-cf clear"></div>
+
+    <!-- Books and articles -->
+    <div class="row" id="apps">        
+        <div class="offset-by-one five columns">
+            <h2>Books</h2>
+            
+            <div class="u-cf"></div>
+            
+            <img src="assets/media/apps/headspace.jpg" class="u-pull-left" />
+            
+            <h3>Headspace</h3>
+            <p>Headspace is a digital service that provides guided meditation sessions and mindfulness training. Its content can be accessed online, or via their mobile apps.</p>
+        </div>
+        
+        <div class="offset-by-one five columns">
+            <h2>Articles</h2>
+            
+            <ul>
+                <li><a href="" target="_blank">Article here</a></li>
+            </ul>
         </div>
     </div>
 
