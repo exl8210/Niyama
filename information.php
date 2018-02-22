bc20975b0eece977ea646a13671173c6bb8edfbd
Ethelia Lung <exl8210@rit.edu>
2018-02-21 22:34:05 -0500

    Edited some styling, added in TED screenshots

--- information.php
+++ information.php
@@ -19,14 +19,14 @@
         <div class="u-cf"></div>
         
         <div class="offset-by-one five columns">
-            <img src="assets/media/apps/headspace.jpg" class="u-pull-left" />
+            <img src="assets/media/apps/headspace.jpg" class="u-pull-left info-icon" />
             
             <h3>Headspace</h3>
             <p>Headspace is a digital service that provides guided meditation sessions and mindfulness training. Its content can be accessed online, or via their mobile apps.</p>
         </div>
         
         <div class="offset-by-one five columns">
-            <img src="assets/media/apps/happify.jpg" class="u-pull-left" />
+            <img src="assets/media/apps/happify.jpg" class="u-pull-left info-icon" />
             
             <h3> Happify</h3>
             <p>Happify is the single destination for effective, evidence-based solutions for better emotional health and wellbeing in the 21st century.</p>
@@ -36,45 +36,52 @@
     <div class="u-cf clear"></div>
 
     <!-- Talks -->
-    <div class="row" id="ted-talks">        
+    <div class="row" id="ted-talks-1">        
         <div class="offset-by-one five columns">
-            <h2>Apps</h2>
+            <h2>TED Talks</h2>
         </div>
         
         <div class="u-cf"></div>
         
         <div class="offset-by-one three columns">
-            <img src="assets/media/headspace.jpg" />
+            <a href="https://www.ted.com/talks/martin_seligman_on_the_state_of_psychology" target="_blank">
+            <img src="assets/media/ted-talks/seligman.png" class="ted-screen" />
             
-            <h3>The new era of positive psychology</h3>
+            <h3>The new era of positive psychology</h3></a>
             <p>Martin Seligman talks about psychology -- as a field of study and as it works one-on-one with each patient and each practitioner. As it moves beyond a focus on disease, what can modern psychology help us to become?</p>
         </div>
         
         <div class="offset-by-one three columns">
-            <img src="assets/media/happify.jpg" />
+            <a href="https://www.ted.com/talks/dan_gilbert_asks_why_are_we_happy" target="_blank">
+            <img src="assets/media/ted-talks/gilbert.png" class="ted-screen" />
             
-            <h3>The surprising science of happiness</h3>
+            <h3>The surprising science of happiness</h3></a>
             <p>Dan Gilbert, author of "Stumbling on Happiness," challenges the idea that we'll be miserable if we don't get what we want. Our "psychological immune system" lets us feel truly happy even when things don't go as planned.</p>
         </div>
         
         <div class="offset-by-one three columns">
-            <img src="assets/media/happify.jpg" />
+            <a href="https://www.tedmed.com/talks/show?id=526817" target="_blank">
+            <img src="assets/media/ted-talks/stellar.png" class="ted-screen" />
             
-            <h3>The positive effects of positive emotions</h3>
+            <h3>The positive effects of positive emotions</h3></a>
             <p>Positive psychology researcher Jennifer Stellar illuminates how truly awe-inspiring experiences may mean more than we realize for our physical and mental health.</p>
         </div>
+    </div>
         
+    <div class="row" id="ted-talks-2">
         <div class="offset-by-one three columns">
-            <img src="assets/media/happify.jpg" />
+            <a href="https://www.ted.com/talks/drew_dudley_everyday_leadership" target="_blank">
+            <img src="assets/media/ted-talks/dudley.png" class="ted-screen" />
             
-            <h3>Everyday leadership</h3>
+            <h3>Everyday leadership</h3></a>
             <p>We have all changed someone's life -- usually without even realizing it. In this funny talk, Drew Dudley calls on all of us to celebrate leadership as the everyday act of improving each other's lives.</p>
         </div>
         
         <div class="offset-by-one three columns">
-            <img src="assets/media/happify.jpg" />
+            <a href="https://www.ted.com/talks/kio_stark_why_you_should_talk_to_strangers" target="_blank">
+            <img src="assets/media/ted-talks/stark.png" class="ted-screen" />
             
-            <h3>Why you should talk to strangers</h3>
+            <h3>Why you should talk to strangers</h3></a>
             <p>"When you talk to strangers, you're making beautiful interruptions into the expected narrative of your daily life -- and theirs," says Kio Stark. In this delightful talk, Stark explores the overlooked benefits of pushing past our default discomfort when it comes to strangers and embracing those fleeting but profoundly beautiful moments of genuine connection.</p>
         </div>
     </div>
@@ -88,7 +95,7 @@
             
             <div class="u-cf"></div>
             
-            <img src="assets/media/apps/headspace.jpg" class="u-pull-left" />
+            <img src="assets/media/apps/headspace.jpg" class="u-pull-left info-icon" />
             
             <h3>Headspace</h3>
             <p>Headspace is a digital service that provides guided meditation sessions and mindfulness training. Its content can be accessed online, or via their mobile apps.</p>
