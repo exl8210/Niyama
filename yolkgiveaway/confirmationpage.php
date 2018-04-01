bed2004638eafdd7b1b0cd0dd5783de70c9dc18c
Ethelia Lung <exl8210@rit.edu>
2018-04-01 13:42:56 -0400

    Touched up styles of yolk giveaway
    Touched up, reusing styles from the main stylesheet to reduce redundancy.

--- yolkgiveaway/confirmationpage.php
+++ yolkgiveaway/confirmationpage.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		
-			<meta charset="utf-8">
-			<link rel="stylesheet" href="emailpage.css">
-			<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-			<link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
-			<title>Niyama Email Form</title>
-	</head>
-	<body class="confirm">
-		
-			<main>
-				<div class="box2">
-			
-				<h1>Thank you for entering!</h1>
-				<p id="specialtext">The winner will be contacted through the provided email and all participants will receive
-				an update on the exhibit!</p>
-				<a href="index.php"><button type="button" name="Back">Back</button></a>
-				</div>
-			
-			</main>
-		
-	</body>
+
+<head>
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="emailpage.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+    <title>Niyama Email Form</title>
+</head>
+
+<body class="confirm">
+
+    <main>
+        <div class="box2">
+            <h1>Thank you for entering!</h1>
+            <p>The winner will be contacted through the provided email and all participants will receive an update on the exhibit!</p>
+            
+            <br /><br />
+            
+            <a href="index.php">
+                <button class="button" type="button" name="Back">Back</button>
+            </a>
+        </div>
+
+    </main>
+
+</body>
+
 </html>
