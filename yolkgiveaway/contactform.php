34a24048f008d250c1bf71c232e25742e59ea118
Ethelia Lung <exl8210@rit.edu>
2018-04-02 22:19:28 -0400

    Fixed email form, added quotes
    I fixed the email form - it was a matter of following some instructions and renaming an attribute's value. Then I added some quotes and shifted some quotes around to include self-care quotes.

--- yolkgiveaway/contactform.php
+++ yolkgiveaway/contactform.php
@@ -1,12 +1,38 @@
 <?php
-	//if form was submitted
-    if(isset($_POST['submit'])) {
+    // reCaptcha info
+    $secret = "6LcALlAUAAAAAFU4RNbrbZPtIR2dH_NnfilHHm80";
+    $remoteip = $_SERVER["REMOTE_ADDR"];
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+
+    // Form info
+    $name = $_POST["name"];
+    $email = $_POST["mail"];
+    $response = $_POST["g-recaptcha-response"];
+
+    // Curl Request
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, array(
+        'secret' => $secret,
+        'response' => $response,
+        'remoteip' => $remoteip
+        ));
+    $curlData = curl_exec($curl);
+    curl_close($curl);
+
+    // Parse data
+    $recaptcha = json_decode($curlData, true);
+
+    if ($recaptcha["success"]) {
         $msg = 'Name: ' .$_POST['name'] ."\n"
-                .'Email: ' .$_POST['mail'] ."\n";
+            .'Email: ' .$_POST['mail'] ."\n";
         mail('niyama2018@gmail.com', 'Silicone Yolk Entry', $msg);
         header('location: confirmationpage.php');
-    } else {
-        echo "Something went wrong";
+    }
+    else {
+        echo "Failure";
         exit(0);
     }
 ?>
--- yolkgiveaway/index.php
+++ yolkgiveaway/index.php
@@ -15,29 +15,25 @@
     <link rel="icon" type="image/png" href="../assets/media/brand/favicon-96x96.png" sizes="96x96">
     <link rel="icon" type="image/png" href="../assets/media/brand/favicon-194x194.png" sizes="194x194">
 
-    <!-- reCAPTCHA -->
-    <script src="https://www.google.com/recaptcha/api.js?onload=onLoad&render=explicit" async defer></script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <!-- -->
+    <!-- reCAPTCHA -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
     <script type="text/javascript">
-        var onLoad = function() {
-            widgetId = grecaptcha.render('componentId', {
-                'sitekey': 'yourKey',
-                'callback': onSubmit,
-            });
+        var onSubmit = function(response) {
+            document.getElementById('contact-form').submit(); // send response to your backend service
         };
+
     </script>
-    
+
 </head>
 
 <body class="main">
 
     <main>
-        <form class="contact-form" action="contactform.php" method="post">
-            <div class="g-recaptcha" data-sitekey="yourKey" data-callback="onSubmit" data-size="invisible">
-            </div>
-
+        <form id="contact-form" action="contactform.php" method="post">
             <h1>Enter to win a silicone yolk!</h1>
             <p>Enter your name and email to be entered to win a silicone buddy! If you win you will be asked to provide your address so we can mail you your prize.</p>
 
@@ -53,7 +49,7 @@
             <br>
             <br>
 
-            <button class="button g-recaptcha" type="submit" name="submit" data-sitekey="6LcALlAUAAAAAMhJGDASdaoeIk8txH17AO2Rf0i9">Enter</button>
+            <button class="button g-recaptcha" type="submit" name="submitBtn" data-sitekey="6LcALlAUAAAAAMhJGDASdaoeIk8txH17AO2Rf0i9" data-callback="onSubmit">Enter</button>
 
 
         </form>
